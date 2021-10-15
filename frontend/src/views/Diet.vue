0873148d0e303994c62187a709b7ad65a35ec93a
Ibrahim <ibrahim.naboulsi97@gmail.com>
2021-10-15 03:52:39 +0200

    added radio buttons

--- frontend/src/views/Diet.vue
+++ frontend/src/views/Diet.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h2>DO YOU FOLLOW A SPECIFIC DIET?</h2>
+        <b-form-radio name="radio-size" size="sm">None</b-form-radio>
+        <b-form-radio name="radio-size" size="sm">Pescetarian</b-form-radio>
+        <b-form-radio name="radio-size" size="sm">Vegetarian</b-form-radio>
+        <b-form-radio name="radio-size" size="sm">Paleo</b-form-radio>
+        <b-form-radio name="radio-size" size="sm">Keto</b-form-radio>
+        <b-form-radio name="radio-size" size="sm">Vegan</b-form-radio>
     </div>
 </template>
 
@@ -17,4 +23,7 @@ export default {
 
 <style>
 
+b-form-radio{
+    float:left;
+}
 </style>
