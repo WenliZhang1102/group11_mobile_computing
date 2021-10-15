415741338b4aaecbff7190272a7a2189487d74d1
Ibrahim <ibrahim.naboulsi97@gmail.com>
2021-10-15 02:33:58 +0200

    Added all categories

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -19,19 +19,30 @@
     <div class="accordion" role="tablist">
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-1 variant="info">Accordion 1</b-button>
+        <b-button block v-b-toggle.accordion-1 variant="info">Pantry</b-button>
       </b-card-header>
       <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
         <b-card-body>
           <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
           <b-card-text>{{ text }}</b-card-text>
+          <div>
+            <b-form-checkbox
+              id="checkbox-1"
+              v-model="status"
+              name="checkbox-1"
+              value="accepted"
+              unchecked-value="not_accepted"
+            >
+              I accept the terms and use
+            </b-form-checkbox>
+          </div>
         </b-card-body>
       </b-collapse>
     </b-card>
 
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-2 variant="info">Accordion 2</b-button>
+        <b-button block v-b-toggle.accordion-2 variant="info">Eggs and Dairy</b-button>
       </b-card-header>
       <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
         <b-card-body>
@@ -42,7 +53,7 @@
 
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-3 variant="info">Accordion 3</b-button>
+        <b-button block v-b-toggle.accordion-3 variant="info">Fruits and Vegetables</b-button>
       </b-card-header>
       <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
         <b-card-body>
@@ -50,6 +61,51 @@
         </b-card-body>
       </b-collapse>
     </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-4 variant="info">Bread and Bakery</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-5 variant="info">Frozen</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-5" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-6 variant="info">Beverages</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-6" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-7 variant="info">Other</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-7" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
   </div>
   </div>
 </template>
